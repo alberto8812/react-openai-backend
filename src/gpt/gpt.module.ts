21d0beb4dc792e327a401487d9a090371a957131
alberto8812 <cvelascosaavedra@gamil.com>
2024-10-08 22:35:10 -0500

    Refactorizar servicio GptService y agregar servicio ProConsDisCusseOpenaiService
    Se refactorizó el servicio GptService para agregar el servicio ProConsDisCusseOpenaiService. Ahora el servicio GptService tiene un nuevo método llamado prosConsDicusser que utiliza el servicio ProConsDisCusseOpenaiService para generar una respuesta con pros y contras en formato markdown. También se agregaron las importaciones necesarias en los archivos correspondientes.

--- src/gpt/gpt.module.ts
+++ src/gpt/gpt.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
 import { GptController } from './infrastructure/gpt.controller';
-import { GptService } from './aplication/gpt.service';
 import { PrismaService } from 'src/common/infractructure/config-dataBase';
 import { OpenAiModule } from './infrastructure/openai/openai.module';
+import { ProConsDisCusseOpenaiService } from './infrastructure/openai';
+import { GptService } from './aplication/gpt.service';
 
 
 
 @Module({
   controllers: [GptController],
-  providers: [GptService, PrismaService],
+  providers: [GptService, ProConsDisCusseOpenaiService, PrismaService],
   imports: [OpenAiModule]
 })
 export class GptModule { }
